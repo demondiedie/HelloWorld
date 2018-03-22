4f0508e049e159bc4762d8d0ebfaebd8f2265528
demondiedie <tiepnv281@gmail.com>
2018-03-23 03:00:55 +0900

    pythonCanBan
    4 phep tinh can ban
    cach tao comment

---
+++ LearnXinYminutes.py
@@ -0,0 +1,24 @@
+"""Chú thích: tất cả sẽ đc viết thành lệch,chạy trực tiếp khi compile bằng python.
+    Mình sẽ dùng bản Python 3.
+"""
+# Dùng kí tự (#) để tạo comment
+
+""" Dùng 3 dấu ngoặc kép (") để
+    đánh trên nhiều dòng.
+    Hoặc, có thể sử dụng để comment
+"""
+
+############################################################
+# 1.Khởi đầu và tính toán trong Python
+############################################################
+
+# Số
+print(3)            # in ra số 3
+
+# 4 phép tính cơ bản
+print(1+1)          # 2
+print(8-1)          # 7
+print(10*2)         # 20
+print(35/5)         # 7.0
+
+# 
