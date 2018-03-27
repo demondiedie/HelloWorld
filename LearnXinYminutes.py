eb41e894b2ceb45354831ce3c10ef9b725bfc5dd
demondiedie <tiepnv281@gmail.com>
2018-03-27 11:11:49 +0900

    cap nhat
    cap nhat

--- LearnXinYminutes.py
+++ LearnXinYminutes.py
@@ -1,5 +1,6 @@
 """Chú thích: tất cả sẽ đc viết thành lệch,chạy trực tiếp khi compile bằng python.
     Mình sẽ dùng bản Python 3.
+    Được dịch từ trang LearnXinYminutes.com
 """
 # Dùng kí tự (#) để tạo comment
 
@@ -13,12 +14,36 @@
 ############################################################
 
 # Số
-print(3)            # in ra số 3
+print(3)  # in ra số 3
 
 # 4 phép tính cơ bản
-print(1+1)          # 2
-print(8-1)          # 7
-print(10*2)         # 20
-print(35/5)         # 7.0
+print(1 + 1)  # 2
+print(8 - 1)  # 7
+print(10 * 2)  # 20
+print(35 / 5)  # 7.0
 
-# 
+# Kết quả của phép chia số nguyên thì sẽ bị lược mất phần số thập phân
+print(5 // 3)  # 1
+print(5.0 // 3.0)  # 1.0
+print(-5 // 3)  # -2
+print(-5.0 // 3.0)  # -2.0
+
+# Phép chia bình thường thì sẽ hiển thị phần thập phân
+print(10.0 / 3)  # 3.3333333335
+print(10 / 3)  # 3.3333333335  (cả 3 giống nhau)
+print(10 / 3.0)  # 3.3333333335
+
+# Phép chia lấy dư %
+print(7 % 3)  # 1
+print(5 % 3)  # 2
+
+# Phép mũ
+print(2 ** 4)  # 2^4 = 16
+
+# Giá trị Boolean(Chú ý: chữ cái đầu viết hoa)
+print(True)  # True
+print(False)  # False
+
+# not (ý phủ định)
+print(not True) # False
+print(not False)    #True
